1a105877a8f53cc6c710d6454ce1c25b481015a6
Harish Navnit <harishnavnit@gmail.com>
2018-10-07 16:52:35 +0530

    Repo-reorganization.

--- src/config/settings.py
+++ src/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'orara.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'orara.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
