7f5fac8c6f2d8f412ccfd264b918e1f8351c3f71
Harish Navnit <harishnavnit@gmail.com>
2019-04-20 15:46:25 +0530

    Switch database engine to PostgreSQL

--- src/config/settings.py
+++ src/config/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orara',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'ATOMIC_REQUESTS': True
     }
 }
 
